f09aff3dd9d3d46d78e1636157437863e3c852d1
cidarosa <lucita.rosa@gmail.com>
2023-12-12 18:43:23 -0300

    PAGE e JOIN FETCH

--- src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
+++ src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
@@ -3,6 +3,8 @@ package com.devsuperior.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,15 @@ public class EmployeeController {
 //	public List<Employee> findAll() {
 //		return employeeRepository.findAll();
 //	}
+
+//    @GetMapping
+//    public List<Employee> findAll() {
+//        return employeeRepository.searchAll();
+//    }
+
+    // com paginação
     @GetMapping
-    public List<Employee> findAll() {
-        return employeeRepository.searchAll();
+    public Page<Employee> findAll(Pageable pageable) {
+        return employeeRepository.searchAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/devsuperior/demo/repositories/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.devsuperior.demo.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.demo.entities.Employee;
@@ -9,9 +11,14 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //resolvendo o problema consulta N+1
-    @Query(value = "SELECT obj FROM Employee obj JOIN FETCH obj.department") // department - atributo da classe
+    @Query(value = "SELECT obj FROM Employee obj JOIN FETCH obj.department")
+    // department - atributo da classe
     List<Employee> searchAll();
 
+    // com paginação
+    @Query(value = "SELECT obj FROM Employee obj JOIN FETCH obj.department",
+            countQuery = "SELECT COUNT(obj) FROM Employee obj JOIN obj.department")
+    Page<Employee> searchAll(Pageable pageable);
 
 
 }
